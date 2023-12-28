603031a8d27c70b0bb1acb48c88a156b7dd9fdd6
Andrei James Alejandro <squishycaptain09@gmail.com>
2023-12-28 18:33:43 +0800

    update order status front end and readability

--- App/pages/order_status.php
+++ App/pages/order_status.php
@@ -6,55 +6,61 @@
   <title>Order Status</title>
   <link rel="stylesheet" href="../css/order_status.css">
 </head>
+
 <body>
-<div class="header">
-  <div class="back-button">
-    <button><a href="../homepage/homepage.php"><img src="../ASSETS/Back.png"></a></button>
-  </div>
-  <div class="title">
-    <h1>Order Status</h1>
-  </div>
-  <div class="logo">
-    <img src="../ASSETS/Company Name.png">
-  </div>
-</div>
-<div class="nav">
-    <ul>
-      <li><a href="../homepage/homepage.php">HOME</a></li>
-      <li><a href="../order details/status.php">ORDER STATUS</a></li>
-    </ul>
-</div>
-<div class="timeline">
-  <div class="container-left">
-    <div class="content">
-      <h2>Order Placed</h2>
-      <p>Your order has been placed successfully.</p>
+  <div class="navbar">
+        <div class="logo">WIBS</div>
+        <div class="nav-links">
+            <a href="#home">Home</a>
+            <a href="#orderstatus">Order Status</a>
+            <a href="#mycart">My Cart</a>
+        </div>
+        <div class="profile-name"><strong>AJ Alejandro</strong></div>
+    </div>
+
+    <div class="status-text">
+      <h1>Your Current Delivery Status</h1>
+    </div>
+
+  <div class="timeline">
+    <div class="container-left">
+      <div class="content">
+        <h2>Order Placed</h2>
+        <p>Your order has been placed successfully.</p>
+      </div>
+    </div>
+
+    <div class="container-right">
+      <div class="content">
+        <h2>Order Confirmed</h2>
+        <p>We are preparing your items to be shipped.</p>
+      </div>
+    </div>
+
+    <div class="container-left">
+      <div class="content">
+        <h2>Shipped</h2>
+        <p>Your order is on the way.</p>
+      </div>
+    </div>
+
+    <div class="container-right">
+      <div class="content">
+        <h2>Out for Delivery</h2>
+        <p>Your order is out for delivery.</p>
+      </div>
+    </div>
+    
+    <div class="container-left">
+      <div class="content">
+        <h2>Delivered</h2>
+        <p>Your order has been delivered. Enjoy!</p>
+      </div>
     </div>
   </div>
-  <div class="container-right">
-    <div class="content">
-      <h2>Order Confirmed</h2>
-      <p>We are preparing your items to be shipped.</p>
-    </div>
-  </div>
-  <div class="container-left">
-    <div class="content">
-      <h2>Shipped</h2>
-      <p>Your order is on the way.</p>
-    </div>
-  </div>
-  <div class="container-right">
-    <div class="content">
-      <h2>Out for Delivery</h2>
-      <p>Your order is out for delivery.</p>
-    </div>
-  </div>
-  <div class="container-left">
-    <div class="content">
-      <h2>Delivered</h2>
-      <p>Your order has been delivered. Enjoy!</p>
-    </div>
-  </div>
-</div>
+  
+  <footer class="site-footer">
+        <p>&copy; 2023 WIBS. All rights reserved.</p>
+  </footer>
 </body>
 </html>
