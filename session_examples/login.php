4cdbc62a573109cfbeb5251fb83111aa8d5f4909
Andrei James Alejandro <squishycaptain09@gmail.com>
2024-01-06 07:41:13 +0800

    add session examples

---
+++ session_examples/login.php
@@ -0,0 +1,84 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+$conn = mysqli_connect('127.0.0.1:3306','u733671518_wibs','|4Kh/3XYD','u733671518_project');
+
+session_start();
+
+$errors = array();
+
+if (isset($_POST['submit'])) {
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = md5($_POST['password']);
+ 
+    $select = "SELECT * FROM customer WHERE email = ? AND password = ?";
+    $stmt = mysqli_prepare($conn, $select);
+    mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+        $_SESSION['user_name'] = $row['name'];
+        header('location: ../homepage/homepage.php');
+        exit();
+    } else {
+        $errors[] = 'Incorrect email or password!';
+    }
+}
+?>	
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/login_styles.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+    <title>WIBS Login</title>
+</head>
+
+<body>
+    <header class="header">
+        <a href="../landing_page/landing.php"><img src="../ASSETS/back.png" alt="asdasdasd"></a>
+        <div class="header-title"><img src="../ASSETS/Company Name.png" alt="pciture"></div>
+    </header>
+
+    <div class="content">   
+        <div class="content-image">
+            <img src="../ASSETS/Red haired man with raised hand.png" alt="ASDASd">
+        </div>
+        <div class="content-form">
+            <form action="" method="post">
+                <h1>Sign In</h1>
+                <?php
+                if (!empty($errors)) {
+                    echo '<div class="error-msg">' . implode('<br>', $errors) . '</div>';
+                }
+                ?>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="email" placeholder="Input Email" required />
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" placeholder="Input Password" required />
+                </div>
+                <div class="form-group">
+                    <button id="submit" type="submit" name="submit">Sign In</button>
+                </div>
+                <div id="loginResult"></div>
+                <div class="signup-link">
+                    Don't have an account? <a href="../signup/signup.php">Sign Up</a>.
+                </div>
+            </form>
+        </div>
+    </div>
+    <footer>
+        <img src="../ASSETS/open cardboard box.png" alt="">
+    </footer>
+</body>
+<script src="script.js"></script>
+
+</html>
---
+++ session_examples/signup.php
@@ -0,0 +1,120 @@
+<?php
+$conn = mysqli_connect('127.0.0.1:3306', 'u733671518_wibs', '|4Kh/3XYD', 'u733671518_project');
+
+$error = array();
+
+if (isset($_POST['submit'])) {
+
+  $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+  $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+  $name = mysqli_real_escape_string($conn, $_POST['username']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $pass = md5($_POST['password']);
+  $cpass = md5($_POST['confirm_password']);
+  $address = mysqli_real_escape_string($conn, $_POST['address']);
+
+  $select = "SELECT * FROM customer WHERE email = '$email'";
+
+  $result = mysqli_query($conn, $select);
+
+  if (mysqli_num_rows($result) > 0) {
+    $error[] = 'User already exists!';
+  } else {
+    if ($pass != $cpass) {
+      $error[] = 'Passwords do not match!';
+    } else {
+      $insert = "INSERT INTO customer(firstname, lastname, name, email, password, address) VALUES('$firstname', '$lastname', '$name', '$email', '$pass', '$address')";
+      mysqli_query($conn, $insert);
+      header('location: ../login/login.php');
+      exit;
+    }
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../css/signup_styles.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+  <title>WIBS Sign Up</title>
+</head>
+
+<body>
+  <header class="header">
+    <div class="header-icon"><a href="../landing_page/landing.html">Back</a></div>
+    <div class="header-title"><img src="../ASSETS/Company Name.png" alt=""></div>
+  </header>
+
+  <div class="content">
+    <div class="content-image">
+      <img src="../ASSETS/Man presenting business idea on laptop.png" alt="">
+    </div>
+
+    <?php
+    if (!empty($error)) {
+      foreach ($error as $err) {
+        echo '<span class="error-msg">' . $err . '</span>';
+      }
+    }
+    ?>
+
+    <div class="content-form">
+      <form id="signup-form" method="post">
+        <h1>Sign Up</h1>
+
+        <div class="form-group">
+          <label for="firstname">First Name</label>
+          <input type="text" id="firstname" name="firstname" placeholder="Input First Name" />
+        </div>
+
+        <div class="form-group">
+          <label for="lastname">Last Name</label>
+          <input type="text" id="lastname" name="lastname" placeholder="Input Last Name" />
+        </div>
+
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" placeholder="Input Username" />
+        </div>
+
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="text" id="email" name="email" placeholder="Input Email" />
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Input Password" />
+        </div>
+
+        <div class="form-group">
+          <label for="confirm_password">Confirm Password</label>
+          <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" />
+        </div>
+
+        <div class="form-group">
+          <label for="address">Address</label>
+          <input type="text" id="address" name="address" placeholder="Input Address" />
+        </div>
+
+        <div class="form-group">
+          <button id="signup" type="submit" name="submit">Sign Up</button>
+        </div>
+        <div id="signupResult"></div>
+        <div class="signup-link">
+          Already have an account? <a href="../login/login.php">Sign In</a>.
+        </div>
+      </form>
+    </div>
+  </div>
+  <footer>
+    <img src="../ASSETS/Blue shopping bag in air.png" alt="">
+  </footer>
+</body>
+<script src="script.js"></script>
+
+</html>
